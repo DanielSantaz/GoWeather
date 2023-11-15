8492023a865299724af43f6c9230bd3cbf80857e
DanielSantaz <danielsantaz@gmail.com>
2023-11-15 12:53:56 -0500

    Go Project added go.mod, html for index & weather, finished up logic

--- main.go
+++ main.go
@@ -1,1 +1,134 @@
-package goweather
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+    r := gin.Default()
+	r.LoadHTMLGlob("views/*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+    r.GET("/weather", func(c *gin.Context) {
+   	 city := c.Query("city")
+   	 latlong, err := getLatLong(city)
+   	 if err != nil {
+   		 c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+   		 return
+   	 }
+
+   	 weather, err := getWeather(*latlong)
+   	 if err != nil {
+   		 c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+   		 return
+   	 }
+
+   	 weatherDisplay, err := extractWeatherData(city, weather)
+    if err != nil {
+   	 c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+   	 return
+    }
+    c.HTML(http.StatusOK, "weather.html", weatherDisplay)
+    })
+
+    r.Run()
+}
+
+func getLatLong(city string) (*LatLong, error) {
+	endpoint := fmt.Sprintf("https://geocoding-api.open-meteo.com/v1/search?name=%s&count=1&language=en&format=json", url.QueryEscape(city))
+	resp, err := http.Get(endpoint)
+	if err != nil {
+		return nil, fmt.Errorf("error making request to Geo API: %w", err)
+	}
+	defer resp.Body.Close()
+
+	var response GeoResponse
+	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+		return nil, fmt.Errorf("error decoding response: %w", err)
+	}
+
+	if len(response.Results) < 1 {
+		return nil, errors.New("no results found")
+	}
+
+	return &response.Results[0], nil
+}
+
+func getWeather(latLong LatLong) (string, error) {
+	endpoint := fmt.Sprintf("https://api.open-meteo.com/v1/forecast?latitude=%.6f&longitude=%.6f&hourly=temperature_2m", latLong.Latitude, latLong.Longitude)
+	resp, err := http.Get(endpoint)
+	if err != nil {
+		return "", fmt.Errorf("error making request to Weather API: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("error reading response body: %w", err)
+	}
+
+	return string(body), nil
+}
+func extractWeatherData(city string, rawWeather string) (WeatherDisplay, error) {
+    var weatherResponse WeatherResponse
+    if err := json.Unmarshal([]byte(rawWeather), &weatherResponse); err != nil {
+   	 return WeatherDisplay{}, fmt.Errorf("error decoding weather response: %w", err)
+    }
+
+    var forecasts []Forecast
+    for i, t := range weatherResponse.Hourly.Time {
+		date, err := time.Parse("2006-01-02T15:04", t)
+   	 if err != nil {
+   		 return WeatherDisplay{}, err
+   	 }
+   	 forecast := Forecast{
+   		 Date:    	date.Format("Mon 15:04"),
+   		 Temperature: fmt.Sprintf("%.1f°C", weatherResponse.Hourly.Temperature2m[i]),
+   	 }
+   	 forecasts = append(forecasts, forecast)
+    }
+    return WeatherDisplay{
+   	 City:  	city,
+   	 Forecasts: forecasts,
+    }, nil
+}
+
+// structs
+type WeatherData struct {}
+type WeatherResponse struct {
+    Latitude  float64 `json:"latitude"`
+    Longitude float64 `json:"longitude"`
+    Timezone  string  `json:"timezone"`
+    Hourly	struct {
+   	 Time      	[]string  `json:"time"`
+   	 Temperature2m []float64 `json:"temperature_2m"`
+    } `json:"hourly"`
+}
+
+type WeatherDisplay struct {
+    City  	string
+    Forecasts []Forecast
+}
+
+type Forecast struct {
+    Date    	string
+    Temperature string
+}
+type GeoResponse struct {
+	// A list of results; we only need the first one
+	Results []LatLong `json:"results"`
+}
+
+type LatLong struct {
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
